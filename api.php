c631e4af7152d12b526c14564000dac110b8a06a
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2020-01-29 06:33:09 -0500

    Update api.php

--- api.php
+++ api.php
@@ -4,22 +4,55 @@
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
  
-    //this is a sample query which gets some data, the order by part shuffles the results
-    //the limit 0, 10 takes the first 10 results.
-    // you might want to consider taking more results, implementing "pagination", 
-    // ordering by rank, etc.
-    $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 10";
+    $count = 1;
+    $binaryVar = decbin($count);
+
+    $receiver = $_GET['receiver'];
+
+    $rack = $receiver;
+    $sizeOfRack = strlen($rack);
+
+    $finalRackArray= array();
+    $outerCount = 0;
+
+    while(strlen(decbin($count)) < $sizeOfRack + 1){
+        $binaryVar = decbin($count);
+        $binaryVar = strrev($binaryVar);
+        $count ++;
+        $tempArray = "";
+        for ($x = 0; $x < strlen($binaryVar); $x++) {
+            if($binaryVar[$x] == "1" ){
+                $tempArray = $tempArray.$rack[$x];
+            }
+           
+        }
+        
+        $query = "SELECT words FROM racks WHERE rack = \"$tempArray\"";
+        $statement = $dbhandle->prepare($query);
+        $statement->execute();
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
+        if($results == array()){
+            
+        }
+        else{
+            $finalResults = array();
+            
+            foreach ($results[0] as $values){
+                $finalResults = $values;
+            }
+            
+            $explodedArray = explode("@@", $finalResults);
+            
+            for($i = 0; $i < count($explodedArray); $i++){
+                array_push($finalRackArray,$explodedArray[$i]);
+            }
+        }
+    }
+
+   
     
-    //this next line could actually be used to provide user_given input to the query to 
-    //avoid SQL injection attacks
-    $statement = $dbhandle->prepare($query);
-    $statement->execute();
-    
-    //The results of the query are typically many rows of data
-    //there are several ways of getting the data out, iterating row by row,
-    //I chose to get associative arrays inside of a big array
-    //this will naturally create a pleasant array of JSON data when I echo in a couple lines
-    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $receiver = $_GET['receiver'];
     
     //this part is perhaps overkill but I wanted to set the HTTP headers and status code
     //making to this line means everything was great with this request
