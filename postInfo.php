999d040799d28bab1b2ed576a16ab77e56b5bcbf
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-04-28 08:31:32 -0400

    Update postInfo.php

--- postInfo.php
+++ postInfo.php
@@ -14,15 +14,16 @@ if (!$dbhandle) die ($error);
 $verb = $_SERVER["REQUEST_METHOD"];
 
 if ($verb === "POST"){
+	console.log("Made it to POST");
 	$sql =<<<<EOF
 		INSERT INTO Posts (User,PostNumber,Time,Likes,Data)
 		VALUES ($_SESSION["usrName"], $_SESSION["postCount"], $_SESSION["postTime"], $_SESSION["postLikes"], $_SESSION["postData"]);
 	EOF;
 	$ret = $dbhandle->exec($sql);
     if(!$ret) {
-       echo $dbhandle->lastErrorMsg();
+       console.log($dbhandle->lastErrorMsg());
     } else {
-       echo "Records created successfully\n";
+       console.log("Records created successfully");
    }
    $dbhandle->close();
    $_SESSION["usrName"] = $_POST['receiver_name'];
