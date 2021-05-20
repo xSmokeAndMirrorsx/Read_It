1a5627bf3c87a678d076f4e51f90a39d5929e667
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-05-20 16:42:33 -0400

    Update cheatsheet.php

--- cheatsheet.php
+++ cheatsheet.php
@@ -7,15 +7,11 @@ if ($verb === "POST"){
     if($_POST['receiver_name'] != ""){
         $_SESSION["name"] = $_POST['receiver_name'];
     }
-    $_SESSION["rack"] = $_POST['receiver_name'];
-    $_SESSION["wordList"] = $_POST['receiver_name'];
-    $_SESSION["correctGuesses"] = $_POST['receiver_name'];
-    $_SESSION["wordLengthCount"] = $_POST['receiver_name'];
 }
 else if ($verb === "GET"){
     header('HTTP/1.1 200 OK');
     header('Content-Type: application/json');
-    $dbSigning_array = array($_SESSION["name"],$_SESSION["rack"],$_SESSION["wordList"],$_SESSION["correctGuesses"],$_SESSION["wordLengthCount"]);
+    $dbSigning_array = array($_SESSION["name"]);
     echo json_encode($dbSigning_array);
 }
 ?>
