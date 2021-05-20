8765b9a66eb50fb45fe6370c4b32f652e6ed1c26
xSmokeAndMirrorsx <46545831+xSmokeAndMirrorsx@users.noreply.github.com>
2021-05-20 19:27:31 -0400

    Update postHandle.php

--- postHandle.php
+++ postHandle.php
@@ -4,25 +4,9 @@ session_start();
 $dbhandle = new PDO("sqlite:posts.sqlite") or die("Failed to open DB");
 if (!$dbhandle) die ($error);
 
-    $count = 1;
-    $binaryVar = decbin($count);
-    $receiver = $_GET['receiver'];
-    $rack = $receiver;
-    $sizeOfRack = strlen($rack);
-    $finalRackArray= array();
-
 $verb = $_SERVER["REQUEST_METHOD"];
 
 if ($verb === "POST"){
-	$_SESSION["usrName"] = $_POST['receiver_name'];
-   	$_SESSION["PostCount"] = $_POST['receiver_name'];
-   	$_SESSION["postTime"] = $_POST['receiver_name'];
-   	$_SESSION["postLikes"] = $_POST['receiver_name'];
-   	$_SESSION["postData"] = $_POST['receiver_name'];
-	$sql =<<<EOF
-		INSERT INTO Posts (User,PostNumber,Time,Likes,Data) VALUES ($_SESSION["usrName"], $_SESSION["postCount"], $_SESSION["postTime"], $_SESSION["postLikes"], $_SESSION["postData"]);
-	EOF;
-	$ret = $dbhandle->exec($sql);
 
 }
 else if ($verb === "GET"){
